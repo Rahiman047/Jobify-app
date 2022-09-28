4c7e8a53d309bc9bad75c2a3f350c95a362c795a
Rahiman <abdurrahiman100@gmail.com>
2022-09-28 15:07:33 +0530

    final

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,9 +4,16 @@ import authenticateUser from "../middleware/auth.js";
 import express from "express";
 
 const router = express.Router();
+import rateLimiter from "express-rate-limit";
 
-router.route("/register").post(register); //public
-router.route("/login").post(login); //public
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, //15 mins
+  max: 15,
+  msg: "Too many requests from this IP address, please try again after 15 mins",
+});
+
+router.route("/register").post(apiLimiter, register); //public
+router.route("/login").post(apiLimiter, login); //public
 router.route("/updateUser").patch(authenticateUser, updateUser); //protected
 
 export default router;
--- server.js
+++ server.js
@@ -4,7 +4,18 @@ import dotenv from "dotenv";
 dotenv.config();
 import "express-async-errors";
 import morgan from "morgan";
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+
 app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
 // DB
 import connectDB from "./db/connect.js";
@@ -26,8 +37,15 @@ app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
